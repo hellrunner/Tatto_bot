0b20b94c039e0cc895828b6f0c88797715caca44
hellrunner <037574@gmail.com>
2023-05-31 23:07:07 +0300

    ready

--- bot.py
+++ bot.py
@@ -6,11 +6,10 @@ token = '5520340219:AAEGg-RH8fboT6ierDkSMXeAVUoWGTWH7AY'
 
 bot = telebot.TeleBot(token)
 
+admin_users = [478991566]
 
-
-
-admin_users = 478991565
-
+maybe =""
+zapis_k_sashe = ""
 
 #начальная страница
 @bot.message_handler(commands=['start'])
@@ -29,54 +28,77 @@ def start(message):
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
 
-    if message.text == '👋':
+    if '👋' in  message.text:
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2) #создание новых кнопок
-        btn1 = types.KeyboardButton('кто я?')
-        btn2 = types.KeyboardButton('Правила')
-        btn3 = types.KeyboardButton('че')
-        btn4 = types.KeyboardButton('вернутся')
-        markup.add(btn1, btn2, btn3, btn4)
+        rtyu = types.KeyboardButton('кто я?')
+        zapis = types.KeyboardButton('Запись')
+        chanal = types.KeyboardButton('Канал')
+        back = types.KeyboardButton('Вернутся')
+        markup.add(rtyu, zapis, chanal, back)
         bot.send_message(message.from_user.id, 'Что вас интересует?', reply_markup=markup) #ответ бота
-
+    
+    elif message.text == 'Вернутся':
+        start(message)
 
     elif message.text == 'кто я?':
         user_id = message.from_user.id
         bot.send_message(message.from_user.id, f'ваш айди {user_id}', parse_mode='Markdown')
 
-    elif message.text == 'Правила':
-        bot.send_message(message.from_user.id, 'нету правил', parse_mode='Markdown')
+    elif message.text == 'Запись':
+        bot.send_message(message.from_user.id, 'Если хотите записаться, то начните сообщение со слов "хочу запись"', parse_mode='Markdown')
 
-    elif message.text == 'че':
+    elif message.text == 'Канал':
         markup = types.InlineKeyboardMarkup()
-        btn5 = types.InlineKeyboardButton(text='Наш сайт', url='https://habr.com/ru/all/')
+        btn5 = types.InlineKeyboardButton(text='тыкать сюды', url='https://t.me/raznyikolkyi')
         markup.add(btn5)
-        bot.send_message(message.from_user.id, "По кнопке ниже можно перейти на сайт хабра", reply_markup = markup)
+        bot.send_message(message.from_user.id, "мой канал", reply_markup = markup)
 
-    elif message.text == 'вернутся':
-        start(message)
-    
     elif message.text == 'я admin':
-        #bot.register_next_step_handler(message, get_mess_from_admin)
-        get_mess_from_admin(message)
+        if message.from_user.id in admin_users:
+            get_mess_from_admin(message)   
+        else: bot.send_message(message.from_user.id, "к сожалению вы не админ")
+    elif message.text == '666':
+        admin_users.append(message.from_user.id)
+        bot.send_message(message.from_user.id, "У вас появился доступ к админке")
     
     
+    elif "хочу запись" in message.text or "Хочу запись" in message.text:
+        global zapis_k_sashe
+        global maybe
+        maybe = f"{zapis_k_sashe} \n @{message.from_user.username} \n {message.text}"
+        get_mess_from_zakas(message)
     
-    
-    
-    
-    
-    
-    
-def get_mess_from_admin(message):
-    
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
-    btn1 = types.KeyboardButton('почему я')
-    btn2 = types.KeyboardButton('почему ns')
-    btn3 = types.KeyboardButton('вернутся')
-    markup.add(btn1,btn2,btn3)
-    bot.send_message(message.from_user.id, 'привет', reply_markup = markup)
-        
-    if message.text == 'вернутся':
+    elif message.text =="да, отправить":
+        zapis_k_sashe = maybe
+        bot.send_message(message.from_user.id, f"Ваша запись успешно отправленна мастеру")
         start(message)
-        
+    
+    elif message.text == "Посмотреть записи":
+        bot.send_message(message.from_user.id, f"Все записи: {zapis_k_sashe}")
+    elif message.text == "Очистить записи":
+        zapis_k_sashe = ""
+        bot.send_message(message.from_user.id, f"Ваши записи успешно очищены")
+
+
+
+
+
+
+def get_mess_from_admin(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    why = types.KeyboardButton('Посмотреть записи')
+    who = types.KeyboardButton('Календарь')
+    backk = types.KeyboardButton('Вернутся')
+    clear_zap = types.KeyboardButton('Очистить записи')
+    markup.add(why,who,backk, clear_zap)
+    bot.send_message(message.from_user.id, 'привет, хозяин', reply_markup = markup)
+            
+def get_mess_from_zakas(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    yes_zakas = types.KeyboardButton('да, отправить')
+    no_zakas = types.KeyboardButton('нет, не отправлять👋')
+    markup.add(yes_zakas, no_zakas)
+    bot.send_message(message.from_user.id, f"Вы уверенны, что хотите иметь запись на сеанс с такими пожеланиями? \n{message.text}", reply_markup= markup)
+
+
 bot.polling(none_stop=True, interval=0)
--- test.py
+++ test.py
@@ -7,49 +7,41 @@ token = '5520340219:AAEGg-RH8fboT6ierDkSMXeAVUoWGTWH7AY'
 bot = telebot.TeleBot(token)
 
 
+
+
+admin_users = 478991565
+
+
+#начальная страница
 @bot.message_handler(commands=['start'])
 def start(message):
+    markup = types.ReplyKeyboardMarkup()
+    buttonA = types.KeyboardButton('A')
+    buttonB = types.KeyboardButton('B')
+    buttonC = types.KeyboardButton('C')
 
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("👋")
-    markup.add(btn1)
-    bot.send_message(message.from_user.id, "👋 Привет! Я бот-помошник!", reply_markup=markup)
+    markup.row(buttonA, buttonB)
+    markup.row(buttonC)
 
+    bot.send_message(message.chat.id, 'It works!', reply_markup=markup)
 
-
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-
-    if message.text == '👋':
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2) #создание новых кнопок
-        btn1 = types.KeyboardButton('кто я?')
-        btn2 = types.KeyboardButton('Правила')
-        btn3 = types.KeyboardButton('че')
-        btn4 = types.KeyboardButton('вернутся')
-        markup.add(btn1, btn2, btn3, btn4)
-        bot.send_message(message.from_user.id, 'Что вас интересует?', reply_markup=markup) #ответ бота
+if message.text == 'A':
+    handle()
 
 
-    elif message.text == 'кто я?':
-        bot.send_message(message.from_user.id, 'Вы пишете и, есдеиго джно прочитать по ' + '[ссылке](https://habr.com/ru/sandbox/start/)', parse_mode='Markdown')
+@bot.callback_query_handler(func=lambda call: True)
+def handle(call):
+    bot.send_message(call.message.chat.id, 'Data: {}'.format(str(call.data)))
+    bot.answer_callback_query(call.id)
 
-    elif message.text == 'Правила':
-        bot.send_message(message.from_user.id, 'нету правил', parse_mode='Markdown')
+@bot.callback_query_handler(lambda call: True)
+def handle(call):
+    bot.send_message(chat_id=call.message.chat.id, message_id=call.message.id, text='It works!')
+    bot.answer_callback_query(call.id)
 
-    elif message.text == 'че':
-        markup = types.InlineKeyboardMarkup()
-        btn5 = types.InlineKeyboardButton(text='Наш сайт', url='https://habr.com/ru/all/')
-        markup.add(btn5)
-        bot.send_message(message.from_user.id, "По кнопке ниже можно перейти на сайт хабра", reply_markup = markup)
+    
+    
+    
+    
 
-    elif message.text == 'вернутся':
-        start(message)
-        
-        
-        
-        
-        
-        
-        
-        
 bot.polling(none_stop=True, interval=0)
